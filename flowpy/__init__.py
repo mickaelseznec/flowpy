7617a86d8ad0966a741aa83a3a715db06fd741a4
Mickaël Seznec <mickael.seznec@gmail.com>
2020-05-12 16:01:30 +0200

    Add module-level documentation

--- flowpy/__init__.py
+++ flowpy/__init__.py
@@ -1,6 +1,16 @@
 """ flowpy
 
-    Contains some utilities for working with optical flows and manipulate associated reference file formats.
+    flowpy
+    ======
+
+    Contains several utilities for working with optical flows:
+        1. flow_read and flow_write let you manipulate flows in .flo and .png format
+        2. flow_to_rgb generates a RGB representation of a flow
+        3. attach_arrows, attach_coord, attach_calibration_pattern provide helper functions to generate beautiful graphs with matplotlib.
+
+    The library handles flow in the HWF format, a numpy.ndarray with 3 dimensions of size [H, W, 2] that hold respectively the height, width and 2d displacement in the (x, y) order.
+
+    Undefined flow is attributed a NaN value.
 """
 
 from .flowpy import (flow_to_rgb, make_colorwheel, calibration_pattern,
