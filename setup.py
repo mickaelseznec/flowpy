4f6016426aa14c9321237dadb558d2295983ee44
Mickaël Seznec <mickael.seznec@gmail.com>
2020-05-15 14:52:21 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(name='flowpy',
-      version='0.3.1',
+      version='0.4',
       description='Tools for working with optical flow',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/mickaelseznec/flowpy',
       author='Mickaël Seznec',
       author_email='flowpy@seznec.xyz',
